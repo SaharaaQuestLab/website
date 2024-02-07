b63fff56a1e33f3201da52057e43db97f3320365
tangkai <1548051084@qq.com>
2024-02-07 23:35:52 +0800

    提交首页面效果

---
+++ src/data/research.ts
@@ -0,0 +1,11 @@
+// 设置联系方式页的meta信息
+export default {
+  titles: {
+   texts:  [
+     'Unveiling',
+     'The Synergy',
+     'of AI And Web3'
+    ],
+    description: 'Sahara Research leads innovations on fair access and ownership to global knowledge capital'
+  }
+ }
