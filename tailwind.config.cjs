6319625db6acd8b6a5affe32773d5262ad226ee7
tangkai <1548051084@qq.com>
2024-02-17 01:42:31 +0800

    统一字号行高

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,32 +9,38 @@ module.exports = {
 			'mobile': { max: '375px' },
 		},
 		fontSize: {
-			h1: ['6.875rem', {
-				lineHeight: '6.375rem'
+			h1: ['110px', {
+				lineHeight: '101.2px'
 			}],
-			h2: ['5.625rem', {
-				lineHeight: '5.125rem'
+			h2: ['90px', {
+				lineHeight: '82.8px'
 			}],
-			h3: ['4.375rem', {
-				lineHeight: '4rem'
+			h3: ['70px', {
+				lineHeight: '64.4px'
 			}],
-			h4: ['3.5rem', {
-				lineHeight: '3.125rem'
+			h4: ['55px', {
+				lineHeight: '50.6px'
 			}],
-			h5: ['2.25rem', {
-				lineHeight: '2.25rem'
+			h5: ['35px', {
+				lineHeight: '37.8px'
 			}],
-			h6: ['1.5rem', {
-				lineHeight: '2.25rem'
+			s1: ['35px', {
+				lineHeight: '50.4px'
 			}],
-			h7: ['1.125rem', {
-				lineHeight: '1.75rem'
+			s2: ['30px', {
+				lineHeight: '43.2px'
 			}],
-			h8: ['0.875rem', {
-				lineHeight: '1.5rem'
+			s3: ['25px', {
+				lineHeight: '36px'
 			}],
-			h9: ['0.75rem', {
-				lineHeight: '1.125rem'
+			base: ['17px', {
+				lineHeight: '26.86px'
+			}],
+			sm: ['15px', {
+				lineHeight: '23.7px'
+			}],
+			xs: ['13px', {
+				lineHeight: '20.54px'
 			}]
 		},
 		colors: {
