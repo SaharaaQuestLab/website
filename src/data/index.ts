6fae86977ab98d92329c1c8297d3c971ecfbc69d
pangwa <pangwa@gmail.com>
2024-02-24 09:16:26 +0800

    fix join waitlist link

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,14 +1,12 @@
 // 设置联系方式页的meta信息
 export default {
- titles: {
-  texts:  [
-    'Your Knowledge.',
-    'Your AI.'
-   ],
-   description: 'Sahara is an AI and blockchain-powered network that unlocks fair and universal access to knowledge capital',
-   button: {
-    text: 'Join waitlist',
-    path: ''
-   }
- }
-}
+  titles: {
+    texts: ["Your Knowledge.", "Your AI."],
+    description:
+      "Sahara is an AI and blockchain-powered network that unlocks fair and universal access to knowledge capital",
+    button: {
+      text: "Join waitlist",
+      url: "https://45253286.hs-sites.com/get-in-touch",
+    },
+  },
+};
