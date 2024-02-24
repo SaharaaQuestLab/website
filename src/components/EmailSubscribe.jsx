4547a73a529a6d51d902d9184f4e353467036010
董志刚 <dongzhigang_bit@126.com>
2024-02-24 08:57:45 +0800

    feat:调整标题轮动动画

--- src/components/EmailSubscribe.jsx
+++ src/components/EmailSubscribe.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import PageMeta from "@/data/footer";
-import { hubSpotUrl, accessToken} from "@/utils/api.utils";
+import { hubSpotUrl, accessToken } from "@/utils/api.utils";
 
-const {freeNewsletter, subscribe } = PageMeta;
+const { freeNewsletter, subscribe } = PageMeta;
 
 export default function EmailSubscribe() {
 
@@ -33,16 +33,16 @@ export default function EmailSubscribe() {
 
   return (
     <div className=" col-span-6 flex justify-end tablet:w-full">
-    <div className="border border-light-100 flex px-4 py-2 bg-dark-400 rounded-xl w-full max-w-[40rem] tablet:max-w-full justify-between">
-      <div className="text-light-200 flex items-center text-sm mobile:text-xs">
-        <img className="w-5 h-5 mr-1.5" src='/public/email.svg' alt="" />
-        <input className="input-bg outline-none" type="text" placeholder={freeNewsletter} value={subscribeEmail} onChange={handleChange} />
-      </div>
-      <div className="cursor-pointer flex text-light-100 text-sm items-center mobile:text-xs" onClick={() => sendEmail()}>
-        {subscribe}
-        <img className="w-5 h-5" src='/public/jump.svg' alt="" />
+      <div className="border border-light-100 flex px-4 py-2 bg-dark-400 rounded-xl w-full max-w-[40rem] tablet:max-w-full justify-between">
+        <div className="text-light-200 flex items-center text-sm mobile:text-xs">
+          <img className="w-5 h-5 mr-1.5" src='/email.svg' alt="" />
+          <input className="input-bg outline-none" type="text" placeholder={freeNewsletter} value={subscribeEmail} onChange={handleChange} />
+        </div>
+        <div className="cursor-pointer flex text-light-100 text-sm items-center mobile:text-xs" onClick={() => sendEmail()}>
+          {subscribe}
+          <img className="w-5 h-5" src='/jump.svg' alt="" />
+        </div>
       </div>
     </div>
-  </div>
   );
 }
