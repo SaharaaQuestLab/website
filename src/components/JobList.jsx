cfd252ec1e157cf98817ede3d973f733682188e7
tangkai <1548051084@qq.com>
2024-02-28 02:22:11 +0800

    select选择优化

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -115,54 +115,68 @@ export default function JobList() {
     let currentX = 0;
 
     draggable.addEventListener('touchstart', (e) => {
-        isDragging = true;
-        startX = e.touches[0].clientX;
-        currentX = draggable.offsetLeft;
+      isDragging = true;
+      startX = e.touches[0].clientX;
+      currentX = draggable.offsetLeft;
     });
 
     el.addEventListener('touchmove', (e) => {
-        if (!isDragging) return;
-        const deltaX = e.touches[0].clientX - startX;
-        let newX = currentX + deltaX;
-        if(-newX >= draggable.clientWidth - el.clientWidth ) {
-          newX = -(draggable.clientWidth - el.clientWidth);
-        }
-        if(newX > 0) {
-          newX = 0
-        }
-        // 使用GSAP来平滑地移动元素
-        gsap.to(draggable, {
-            duration: 0.1,
-            marginLeft: newX,
-            ease: 'power1.out'
-        });
+      if (!isDragging) return;
+      const deltaX = e.touches[0].clientX - startX;
+      let newX = currentX + deltaX;
+      if (-newX >= draggable.clientWidth - el.clientWidth) {
+        newX = -(draggable.clientWidth - el.clientWidth);
+      }
+      if (newX > 0) {
+        newX = 0
+      }
+      // 使用GSAP来平滑地移动元素
+      gsap.to(draggable, {
+        duration: 0.1,
+        marginLeft: newX,
+        ease: 'power1.out'
+      });
 
-        // 更新起始位置以计算接下来的移动距离
-        // startX = newX;
+      // 更新起始位置以计算接下来的移动距离
+      // startX = newX;
     });
 
     document.addEventListener('touchend', () => {
-        isDragging = false;
+      isDragging = false;
     });
   }, []);
+  const select1 = useRef(null);
+  const select2 = useRef(null);
+  const select3 = useRef(null);
 
+  const blur = () => {
+    select1.current.style.overflow = "hidden";
+    select2.current.style.overflow = "hidden";
+    select3.current.style.overflow = "hidden";
+    setTimeout(() => {
+      select1.current.style.overflow = "visible";
+      select2.current.style.overflow = "visible";
+      select3.current.style.overflow = "visible";
+    }, 20);
+  }
   const resetSearch = () => {
     setSubscribeTargetEmploymentType({});
     setSubscribeTargetLocation({});
     setSubscribeTargetDepartment({});
   }
   return (
-    <div className="w-full overflow-hidden" ref={contentWrap}>
+    <div className="w-full" ref={contentWrap}>
       <div className="mt-40 mb-[3.75rem] mobile:mb-7 text-h3 tablet:text-h4 mobile:text-h5 font-[Manrope]">
         <span className="mr-4">Open Positions</span>
         <span className="text-light-300">{subscribeSearchList.length}</span>
       </div>
-      <div className="h-11 mb-[3.75rem] tablet:mb-11 mobile:mb-7 flex gap-x-5 tablet:gap-x-3 min-w-max max-w-max" ref={content}>
-        <div className="border flex-1 px-4 text-sm mobile:text-xs py-2.5 cursor-pointer relative rounded-xl group hover:rounded-b-none hover:border-b-[rgba(0,0,0,0)] border-light-100 flex items-center justify-between">
+      <div className="h-11 mb-[3.75rem] tablet:mb-11 mobile:mb-7 flex gap-x-5 tablet:gap-x-3 min-w-max w-full" ref={content}>
+        <div ref={select1} className="border flex-1 px-4 text-sm mobile:text-xs py-2.5 cursor-pointer relative rounded-xl group border-light-100 flex items-center justify-between">
           {subscribeTargetDepartment.id ? <span className="whitespace-nowrap truncate">{subscribeTargetDepartment.name}</span> : <span className="text-light-200 whitespace-nowrap truncate">Department</span>}<img
             id="icon"
             className="w-5 h-5 rotate-180 group-hover:rotate-0" src={arrowCorner_768_375.src} alt="" />
-          <div className="absolute left-[-1px] top-11 w-[calc(100%+2px)] bg-dark-400 border-b border-l border-r group-hover:block hidden -mt-0.5 border-light-100 rounded-xl rounded-t-none overflow-hidden">
+          <div className="absolute w-full left-[-1px] top-11 h-1"></div>
+          <div onClick={blur} className="absolute left-[-1px] top-12 bg-dark-400 border group-hover:block min-w-full min-h-11 hidden -mt-0.5 border-light-100 rounded-xl overflow-hidden">
             {
               subscribeDepartmentList.map(item => (
                 <div key={item.id} onClick={() => { setSubscribeTargetDepartment(item) }} style={item.id === subscribeTargetDepartment.id ? { display: 'none' } : { display: 'block' }} className="px-4 py-2.5 hover:bg-light-100 hover:text-dark-400 whitespace-nowrap truncate">{item.name}</div>
@@ -170,11 +184,12 @@ export default function JobList() {
             }
           </div>
         </div>
-        <div className="border flex-1 px-4 text-sm mobile:text-xs py-2.5 cursor-pointer relative rounded-xl group hover:rounded-b-none hover:border-b-[rgba(0,0,0,0)] border-light-100 flex items-center justify-between">
+        <div ref={select2} className="border flex-1 px-4 text-sm mobile:text-xs py-2.5 cursor-pointer relative rounded-xl group border-light-100 flex items-center justify-between">
           {subscribeTargetLocation.id ? <span className="whitespace-nowrap truncate">{subscribeTargetLocation.name}</span> : <span className="text-light-200 whitespace-nowrap truncate">Location</span>}<img
             id="icon"
             className="w-5 h-5 rotate-180 group-hover:rotate-0" src={arrowCorner_768_375.src} alt="" />
-          <div className="absolute left-[-1px] top-11 w-[calc(100%+2px)] bg-dark-400 border-b border-l border-r group-hover:block hidden -mt-0.5 border-light-100 rounded-xl rounded-t-none overflow-hidden">
+          <div className="absolute w-full left-[-1px] top-11 h-1"></div>
+          <div onClick={blur} className="absolute left-[-1px] top-12 bg-dark-400 border group-hover:block min-w-full min-h-11 hidden -mt-0.5 border-light-100 rounded-xl overflow-hidden">
             {
               subscribeLocationList.map(item => (
                 <div key={item.id} onClick={() => { setSubscribeTargetLocation(item) }} className="px-4 py-2.5 hover:bg-light-100 hover:text-dark-400 whitespace-nowrap truncate" style={item.id === subscribeTargetLocation.id ? { display: 'none' } : { display: 'block' }}>{item.name}</div>
@@ -182,11 +197,11 @@ export default function JobList() {
             }
           </div>
         </div>
-        <div className="border flex-1 px-4 text-sm mobile:text-xs py-2.5 cursor-pointer relative rounded-xl group hover:rounded-b-none hover:border-b-[rgba(0,0,0,0)] border-light-100 flex items-center justify-between">
+        <div ref={select3} className="border flex-1 px-4 text-sm mobile:text-xs py-2.5 cursor-pointer relative rounded-xl group border-light-100 flex items-center justify-between">
           {subscribeTargetEmploymentType.id ? <span className="whitespace-nowrap truncate">{subscribeTargetEmploymentType.name}</span> : <span className="text-light-200 whitespace-nowrap truncate">Employment Type</span>} <img
             id="icon"
-            className="w-5 h-5 rotate-180 group-hover:rotate-0" src={arrowCorner_768_375.src} alt="" />
-          <div className="absolute left-[-1px] top-11 w-[calc(100%+2px)] bg-dark-400 border-b border-l border-r group-hover:block hidden -mt-0.5 border-light-100 rounded-xl rounded-t-none overflow-hidden">
+            className="w-5 h-5 rotate-180 group-hover:rotate-0" src={arrowCorner_768_375.src} alt="" /><div className="absolute w-full left-[-1px] top-11 h-1"></div>
+          <div onClick={blur} className="absolute left-[-1px] top-12 bg-dark-400 border group-hover:block min-w-full min-h-11 hidden -mt-0.5 border-light-100 rounded-xl overflow-hidden">
             {
               subscribeEmploymentType.map(item => (
                 <div key={item.id} onClick={() => { setSubscribeTargetEmploymentType(item) }} className="px-4 py-2.5 hover:bg-light-100 hover:text-dark-400 whitespace-nowrap truncate" style={item.id === subscribeTargetEmploymentType.id ? { display: 'none' } : { display: 'block' }}>{item.name}</div>
