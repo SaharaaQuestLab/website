ba3721c7b554ea24c4f52d14a198c82f16a58c1b
tangkai <1548051084@qq.com>
2024-02-28 23:02:26 +0800

    表单层级

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -109,12 +109,12 @@ export default function JobList() {
         <span className="text-light-300">{positionList.length}</span>
       </div>
       <div className="min-h-11 mb-[3.75rem] tablet:mb-11 mobile:mb-7 flex gap-x-5 tablet:gap-x-3 gap-y-7 whitespace-pre-wrap max-w-full flex-wrap">
-        <div ref={select1} className="border flex-1 mobile:min-w-[43%] px-4 text-sm mobile:text-xs py-2.5 cursor-pointer relative rounded-xl group border-light-100 flex items-center justify-between">
+        <div ref={select1} className="border flex-1 mobile:min-w-[43%] px-4 text-sm mobile:text-xs py-2.5 cursor-pointer relative z-10 rounded-xl group border-light-100 flex items-center justify-between">
           {selectDepartment.id ? <span className="whitespace-nowrap truncate">{selectDepartment.name}</span> : <span className="text-light-200 whitespace-nowrap truncate">Department</span>}<img
             id="icon"
             className="w-5 h-5 rotate-180 group-hover:rotate-0" src={arrowCorner_768_375.src} alt="" />
           <div className="absolute w-full left-[-1px] top-11 h-1"></div>
-          <div onClick={blur} className="absolute left-[-1px] top-12 bg-dark-400 border group-hover:block min-w-full min-h-11 hidden -mt-0.5 border-light-100 rounded-xl overflow-hidden">
+          <div onClick={blur} className="absolute left-[-1px] top-12 bg-dark-400 border group-hover:block min-w-full min-h-10 hidden -mt-0.5 border-light-100 rounded-xl overflow-hidden">
             {
               departmentList.map(item => (
                 <div key={item.id} onClick={() => { setSelectDepartment(item) }} style={item.id === selectDepartment.id ? { display: 'none' } : { display: 'block' }} className="px-4 py-2.5 hover:bg-light-100 hover:text-dark-400 whitespace-nowrap truncate">{item.name}</div>
@@ -122,12 +122,12 @@ export default function JobList() {
             }
           </div>
         </div>
-        <div ref={select2} className="border flex-1 mobile:min-w-[43%] px-4 text-sm mobile:text-xs py-2.5 cursor-pointer relative rounded-xl group border-light-100 flex items-center justify-between">
+        <div ref={select2} className="border flex-1 mobile:min-w-[43%] px-4 text-sm mobile:text-xs py-2.5 cursor-pointer relative z-10 rounded-xl group border-light-100 flex items-center justify-between">
           {selectLocation.id ? <span className="whitespace-nowrap truncate">{selectLocation.name}</span> : <span className="text-light-200 whitespace-nowrap truncate">Location</span>}<img
             id="icon"
             className="w-5 h-5 rotate-180 group-hover:rotate-0" src={arrowCorner_768_375.src} alt="" />
           <div className="absolute w-full left-[-1px] top-11 h-1"></div>
-          <div onClick={blur} className="absolute left-[-1px] top-12 bg-dark-400 border group-hover:block min-w-full min-h-11 hidden -mt-0.5 border-light-100 rounded-xl overflow-hidden">
+          <div onClick={blur} className="absolute left-[-1px] top-12 bg-dark-400 border group-hover:block min-w-full min-h-10 hidden -mt-0.5 border-light-100 rounded-xl overflow-hidden">
             {
               locationList.map(item => (
                 <div key={item.id} onClick={() => { setSelectLocation(item) }} className="px-4 py-2.5 hover:bg-light-100 hover:text-dark-400 whitespace-nowrap truncate" style={item.id === selectLocation.id ? { display: 'none' } : { display: 'block' }}>{item.name}</div>
@@ -139,7 +139,7 @@ export default function JobList() {
           {selectEmploymentType.id ? <span className="whitespace-nowrap truncate">{selectEmploymentType.name}</span> : <span className="text-light-200 whitespace-nowrap truncate">Employment Type</span>} <img
             id="icon"
             className="w-5 h-5 rotate-180 group-hover:rotate-0" src={arrowCorner_768_375.src} alt="" /><div className="absolute w-full left-[-1px] top-11 h-1"></div>
-          <div onClick={blur} className="absolute left-[-1px] top-12 bg-dark-400 border group-hover:block min-w-full min-h-11 hidden -mt-0.5 border-light-100 rounded-xl overflow-hidden">
+          <div onClick={blur} className="absolute left-[-1px] top-12 bg-dark-400 border group-hover:block min-w-full min-h-10 hidden -mt-0.5 border-light-100 rounded-xl overflow-hidden">
             {
               employmentTypeList.map(item => (
                 <div key={item.id} onClick={() => { setSelectEmploymentType(item) }} className="px-4 py-2.5 hover:bg-light-100 hover:text-dark-400 whitespace-nowrap truncate" style={item.id === selectEmploymentType.id ? { display: 'none' } : { display: 'block' }}>{item.name}</div>
