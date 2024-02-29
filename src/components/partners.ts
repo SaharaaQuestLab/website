0959c810693ced714354040b84b0286c8b214ec6
hexin <2532206203@qq.com>
2024-02-29 17:27:00 +0800

    feat: some ui fixed

--- src/components/partners.ts
+++ src/components/partners.ts
@@ -1,11 +1,16 @@
-import geekcartel from '@/assets/images/partners/geekcartel.jpg';
-import samsung from '@/assets/images/partners/samsung.jpg';
-import nomad from '@/assets/images/partners/nomad.jpg';
-import canonical from '@/assets/images/partners/canonical.jpg';
-import dispersion from '@/assets/images/partners/dispersion.jpg';
-import alumni from '@/assets/images/partners/alumni.jpg';
-import tangent from '@/assets/images/partners/tangent.jpg';
-import motherson from '@/assets/images/partners/motherson.jpg';
-import coho from '@/assets/images/partners/coho.jpg';
-import sandeep from '@/assets/images/partners/sandeep.jpg';
-export default [geekcartel, samsung, nomad, canonical, dispersion, alumni, tangent, motherson, coho, sandeep];
+import partner1 from '@/assets/images/partners/partner1.svg';
+import partner2 from '@/assets/images/partners/partner2.svg';
+import partner3 from '@/assets/images/partners/partner3.svg';
+import partner4 from '@/assets/images/partners/partner4.svg';
+import partner5 from '@/assets/images/partners/partner5.svg';
+import partner6 from '@/assets/images/partners/partner6.svg';
+import partner7 from '@/assets/images/partners/partner7.svg';
+import partner8 from '@/assets/images/partners/partner8.svg';
+import partner9 from '@/assets/images/partners/partner9.svg';
+import partner10 from '@/assets/images/partners/partner10.svg';
+import partner11 from '@/assets/images/partners/partner11.svg';
+import partner12 from '@/assets/images/partners/partner12.svg';
+import partner13 from '@/assets/images/partners/partner13.svg';
+import partner14 from '@/assets/images/partners/partner14.svg';
+
+export default [partner1, partner2, partner3, partner4, partner5, partner6, partner7, partner8, partner9, partner10, partner11, partner12, partner13, partner14];
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,7 +1,7 @@
 // 设置联系方式页的meta信息
 export default {
   titles: {
-    texts: ["Your Knowledge.", "Your AI."],
+    texts: ["Your Knowledge", "Your AI"],
     description:
       "Sahara is an AI and blockchain-powered network that unlocks fair and universal access to knowledge capital",
     button: {
