922454f57b1fa58f83689dd943d43548abaa03ed
pangwa <pangwa@gmail.com>
2024-03-02 11:25:19 +0800

    fix funder links

--- src/components/partners.ts
+++ src/components/partners.ts
@@ -1,31 +1,31 @@
-import partner1 from '@/assets/images/partners/partner1.svg';
-import partner2 from '@/assets/images/partners/partner2.svg';
-import partner3 from '@/assets/images/partners/partner3.svg';
-import partner4 from '@/assets/images/partners/partner4.svg';
-import partner5 from '@/assets/images/partners/partner5.svg';
-import partner6 from '@/assets/images/partners/partner6.svg';
-import partner7 from '@/assets/images/partners/partner7.svg';
-import partner8 from '@/assets/images/partners/partner8.svg';
-import partner9 from '@/assets/images/partners/partner9.svg';
-import partner10 from '@/assets/images/partners/partner10.svg';
-import partner11 from '@/assets/images/partners/partner11.svg';
-import partner12 from '@/assets/images/partners/partner12.svg';
-import partner13 from '@/assets/images/partners/partner13.svg';
-import partner14 from '@/assets/images/partners/partner14.svg';
+import polychain from '@/assets/images/partners/polychain.svg';
+import sequoia from '@/assets/images/partners/sequoia.svg';
+import matrix from '@/assets/images/partners/matrix.svg';
+import dao5 from '@/assets/images/partners/dao5.svg';
+import geekcartel from '@/assets/images/partners/geekcartel.svg';
+import samsung from '@/assets/images/partners/samsung.svg';
+import nomad from '@/assets/images/partners/nomad.svg';
+import canonical from '@/assets/images/partners/canonical.svg';
+import dispersion from '@/assets/images/partners/dispersion.svg';
+import alumniVentures from '@/assets/images/partners/alumni_ventures.svg';
+import tangent from '@/assets/images/partners/tangent.svg';
+import motherson from '@/assets/images/partners/motherson.svg';
+import cohoDeeptech from '@/assets/images/partners/coho_deeptech.svg';
+import sandeepnailwal from '@/assets/images/partners/sandeepnailwal.svg';
 
 export default [
-    {image: partner1, link: 'https://polychain.capital/'}, 
-    {image: partner2, link: 'https://www.hongshan.com/en/'}, 
-    {image: partner3, link: 'https://www.matrixpartners.com.cn/en/'},
-    {image: partner4, link: 'https://dao5.com/'},
-    {image: partner5, link: 'https://www.geekcartel.org/'},
-    {image: partner6, link: 'https://www.samsungnext.com/'},
-    {image: partner7, link: 'https://www.nomadcapital.io/'},
-    {image: partner8, link: 'https://www.canonical.cc/'},
-    {image: partner9, link: 'https://www.motherson.com/'},
-    {image: partner10, link: 'https://dispersion.xyz/'},
-    {image: partner11, link: 'https://www.av.vc/'},
-    {image: partner12, link: 'https://www.tangent.ventures/'},
-    {image: partner13, link: 'https://www.cohovc.com/'},
-    {image: partner14, link: ''},
+    {image: polychain, link: 'https://polychain.capital/'},
+    {image: sequoia, link: 'https://www.hongshan.com/en/'},
+    {image: matrix, link: 'https://www.matrixpartners.com.cn/en/'},
+    {image: dao5, link: 'https://dao5.com/'},
+    {image: geekcartel, link: 'https://www.geekcartel.org/'},
+    {image: samsung, link: 'https://www.samsungnext.com/'},
+    {image: nomad, link: 'https://www.nomadcapital.io/'},
+    {image: canonical, link: 'https://www.canonical.cc/'},
+    {image: dispersion, link: 'https://dispersion.xyz/'},
+    {image: alumniVentures, link: 'https://www.av.vc/'},
+    {image: tangent, link: 'https://www.tangent.ventures/'},
+    {image: motherson, link: 'https://www.motherson.com/'},
+    {image: cohoDeeptech, link: 'https://www.cohovc.com/'},
+    {image: sandeepnailwal, link: 'https://www.linkedin.com/in/sandeep-nailwal-60709a33'},
 ];
