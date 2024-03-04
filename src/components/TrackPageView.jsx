ee8870657d2118319aaa206479c78616809c9fe1
hexin <2532206203@qq.com>
2024-03-04 14:36:22 +0800

    feat: add ga

---
+++ src/components/TrackPageView.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+import ReactGA from "react-ga4";
+
+export default function TrackPageView () {
+
+    useEffect(() => {
+        ReactGA.initialize("G-8JY051HV2D");
+    }, []);
+
+    return null;
+}
